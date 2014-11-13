c7322c65ccf2ceea7938815b16baf22484e2cd33
Matt Carrier <icecreammatt@gmail.com>
2014-11-12 21:02:17 -0800

    Added template to create the markdown files with metadata

--- gallery.go
+++ gallery.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"syscall"
+	"text/template"
+	"time"
 )
+
+var postTemplate string = `---
+title: {{.Title}}
+date: {{.Date}}
+imageName: {{.ImageName}}
+---
+`
+
+type GalleryItem struct {
+	Title     string
+	Date      string
+	ImageName string
+}
 
 func check(e error) {
 	if e != nil {
@@ -15,31 +31,53 @@ func check(e error) {
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage: %s <Path> <Section>\n", os.Args[0])
+	if len(os.Args) < 4 {
+		fmt.Printf("Usage: %s <Source Path> <Destination Section> <Title>\n", os.Args[0])
 		syscall.Exit(1)
 	}
 
-	contentPath := "content/" + os.Args[2] + "/"
+	sourcePath := os.Args[1]
+	section := os.Args[2]
+	title := os.Args[3]
+	contentPath := "content/" + section + "/"
+
 	src, err := os.Stat(contentPath)
 	if err != nil || !src.IsDir() {
 		err = os.Mkdir(contentPath, 0755)
 		check(err)
 	}
 
-	fileInfo, err := ioutil.ReadDir(os.Args[1])
+	fileInfo, err := ioutil.ReadDir(sourcePath)
 	check(err)
 
-	for index, file := range fileInfo {
-		fmt.Printf("%d %s\n", index, file.Name())
-		filePath := contentPath + file.Name() + ".md"
-		fmt.Println(filePath)
-		f, err := os.Create(filePath)
-		check(err)
-		defer f.Close()
-		f.Sync()
-		w := bufio.NewWriter(f)
-		w.WriteString(file.Name())
-		w.Flush()
+	for _, file := range fileInfo {
+		generatePost(file, contentPath, title)
 	}
+}
+
+func generatePost(file os.FileInfo, contentPath string, title string) {
+	galleryItem := GalleryItem{
+		Title:     title,
+		ImageName: file.Name(),
+		Date:      time.Now().String(),
+	}
+
+	var buffer bytes.Buffer
+	generateTemplate(galleryItem, &buffer)
+
+	filePath := contentPath + file.Name() + ".md"
+	f, err := os.Create(filePath)
+	check(err)
+	defer f.Close()
+	f.Sync()
+	w := bufio.NewWriter(f)
+	w.WriteString(buffer.String())
+	w.Flush()
+}
+
+func generateTemplate(galleryItem GalleryItem, buffer *bytes.Buffer) {
+
+	t := template.New("post template")
+	t, _ = t.Parse(postTemplate)
+	t.Execute(buffer, galleryItem)
 }
